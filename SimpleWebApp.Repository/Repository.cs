4e837ebf1264ab7591cb6b1db4e2de8dbcb5ac15
Mehran Abdi <mehranabdi1379@gmail.com>
2023-03-24 08:03:35 +0330

    Adding repository layer

---
+++ SimpleWebApp.Repository/Repository.cs
@@ -0,0 +1,15 @@
+﻿using SimpleWebApp.DataBase;
+using SimpleWebApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWebApp.Repository
+{
+    public class Repository
+    {
+        
+    }
+}
