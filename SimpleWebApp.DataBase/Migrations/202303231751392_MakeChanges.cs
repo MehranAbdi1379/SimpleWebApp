ff1db1d2fc974ccd017de4f279d66b3f87979ac3
Mehran Abdi <mehranabdi1379@gmail.com>
2023-03-24 06:46:45 +0330

    Adding DateTime to the writer model

---
+++ SimpleWebApp.DataBase/Migrations/202303231751392_MakeChanges.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleWebApp.DataBase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MakeChanges : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Writers", "BirthDate", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Writers", "BirthDate");
+        }
+    }
+}
--- SimpleWebApp.Domain/Writer.cs
+++ SimpleWebApp.Domain/Writer.cs
@@ -8,5 +8,5 @@ public class Writer
     public string LastName { get; set; }
     public string UserName { get; set; }
     public string Password { get; set; }
-    public DateOnly BirthDate { get; set; }
+    public DateTime BirthDate { get; set; }
 }
